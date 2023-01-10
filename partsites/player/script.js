29c217a3b8a87a7713b1e220b231042acf44f4c7
MyOwnBrain <christine.gelbhaar@gmail.com>
2023-01-11 00:10:10 +0100

    update player

--- partsites/player/script.js
+++ partsites/player/script.js
@@ -20,7 +20,7 @@ add_submit.addEventListener("click", () => {
     video_info = add_window.querySelectorAll("div input");
     
     if (video_info[1].value.startsWith("https://www.youtube.com/watch?v=")) {
-        video_info[0].value = /[^*a-zA-Z0-9,./#+-]*$/g.test(video_info[0].value) ? "untitled" : video_info[0].value;
+        video_info[0].value = /[a-zA-Z0-9,./#+-]/g.test(video_info[0].value) ? video_info[0].value : "untitled";
         queue_list.innerHTML += `<li data-token="${video_info[1].value.split(/[=&]+/)[1]}" draggable="true">${video_info[0].value}</li>`
     } else {
         video_info[1].classList.add("invalid");
@@ -33,8 +33,40 @@ add_submit.addEventListener("click", () => {
     video_info[1].classList.remove("invalid")
 })
 
-/*
-video.children.iframe.src = "https://www.youtube.com/embed/" + video_link.value.split(/[=&]+/)[1];
+const play = document.getElementById("play");
+const next = document.getElementById("next");
+let song_index = 0;
 
-video.children.video_none.style.display = "none"
-*/
+play.addEventListener("click", () => {
+    video.children.video_none.style.display = "none";
+    updateIframe(song_index);
+})
+
+next.addEventListener("click", () => {
+    song_index++;
+    try {
+        updateIframe(song_index);
+    } catch {
+        video.children.iframe.src = "https://";
+        video.children.video_none.style.display = "block";
+    }
+})
+
+function updateIframe(song_index) {
+    video.children.iframe.src = "https://www.youtube.com/embed/" + queue_list.children[song_index].dataset.token;
+    queue_list.children[song_index].style = `
+        color: var(--placeholder);
+        background: var(--dark-3);
+    `;
+}
+
+const pause_screen_toggle = document.getElementById("pause_screen_toggle");
+const pause_screen = document.getElementById("pause_screen");
+
+pause_screen_toggle.addEventListener("click", () => {
+    pause_screen.style.display = "flex";
+})
+
+pause_screen.addEventListener("dblclick", (e) => {
+    pause_screen.style.display = "none"
+})
