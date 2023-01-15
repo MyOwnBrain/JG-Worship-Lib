03f3828b828ae0a3142620858789134c10611f26
MyOwnBrain <christine.gelbhaar@gmail.com>
2023-01-15 21:50:50 +0100

    Player Update
    [+] Reorder Queue List
    [+] reset Player to beginning (+ Stop Button)
    [+] placeholder color at Add Inputs

--- partsites/player/script.js
+++ partsites/player/script.js
@@ -1,12 +1,12 @@
-const add = document.getElementById("add");
-const add_window = document.getElementById("add_window");
-const add_cancel = document.getElementById("add_cancel");
-const add_submit = document.getElementById("add_submit");
-const queue_list = document.getElementById("queue_list");
-const video = document.getElementById("video");
-let video_info;
-let text;
-let delete_li;
+const add = document.getElementById("add"),
+    add_window = document.getElementById("add_window"),
+    add_cancel = document.getElementById("add_cancel"),
+    add_submit = document.getElementById("add_submit"),
+    video = document.getElementById("video");
+let video_info,
+    text,
+    delete_li;
+    queue_list = document.getElementById("queue_list");
 
 // add
 
@@ -21,7 +21,7 @@ add_cancel.addEventListener("click", () => {
 
 add_submit.addEventListener("click", () => {
     video_info = add_window.querySelectorAll("div input");
-    
+
     if (video_info[1].value.startsWith("https://www.youtube.com/watch?v=")) {
         video_info[0].value = /[a-zA-Z0-9,./#+-]/g.test(video_info[0].value) ? video_info[0].value : "untitled";
         queue_list.innerHTML += `
@@ -40,7 +40,7 @@ add_submit.addEventListener("click", () => {
     video_info[1].value = "";
     video_info[1].classList.remove("invalid");
     defDelete();
-
+    reorderQueue();
 })
 
 // delete form queue
@@ -49,8 +49,41 @@ function defDelete() {
     queue_list.querySelectorAll("li button").forEach((it) => {
         it.addEventListener("click", (e) => {
             e.composedPath()[1].remove();
+            reorderQueue();
         })
     })
+}
+
+// reorder queue
+
+function reorderQueue() {
+    queue_list = document.getElementById("queue_list");
+    Array.prototype.map.call(queue_list.children, (item) => {
+        console.log(item)
+        item.ondrag = handleDrag;
+        item.ondragend = handleDrop;
+    });
+}
+
+reorderQueue();
+
+function handleDrag(item) {
+    const selectedItem = item.target,
+        list = selectedItem.parentNode,
+        x = event.clientX,
+        y = event.clientY;
+
+    selectedItem.classList.add("drag-active");
+    let swapItem = document.elementFromPoint(x, y) === null ? selectedItem : document.elementFromPoint(x, y);
+
+    if (list === swapItem.parentNode) {
+        swapItem = swapItem !== selectedItem.nextSibling ? swapItem : swapItem.nextSibling;
+        list.insertBefore(selectedItem, swapItem);
+    }
+}
+
+function handleDrop(item) {
+    item.target.classList.remove("drag-active");
 }
 
 // play
@@ -60,17 +93,31 @@ const next = document.getElementById("next");
 let song_index = 0;
 
 play.addEventListener("click", () => {
+    if (queue_list.children.length === 0) return;
     video.children.video_none.style.display = "none";
-    updateIframe(song_index);
+    if (song_index === 0) {
+        updateIframe(song_index);
+        play.children[0].src = "../../images/player/stop.png"
+        song_index++;
+    } else {
+        song_index = 0;
+        Array.prototype.map.call(queue_list.children, (item) => {
+            item.style = `
+                color: var(--font-color);
+                background: var(--dark-4);
+            `;
+        })
+        video.children.iframe.src = "https://";
+        video.children.video_none.style.display = "block";
+        play.children[0].src = "../../images/sidebar/player.png";
+    }
 })
 
 next.addEventListener("click", () => {
-    song_index++;
     try {
         updateIframe(song_index);
     } catch {
-        video.children.iframe.src = "https://";
-        video.children.video_none.style.display = "block";
+        play.click()
     }
 })
 
@@ -91,6 +138,6 @@ pause_screen_toggle.addEventListener("click", () => {
     pause_screen.style.display = "flex";
 })
 
-pause_screen.addEventListener("dblclick", (e) => {
+pause_screen.addEventListener("dblclick", () => {
     pause_screen.style.display = "none"
 })
